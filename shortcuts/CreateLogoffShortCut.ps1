9ecbf288f5cc6893d6f0f2d11569e40ef729591b
Daniel Paessens <daniel.paessens@hpe.com>
2026-01-21 14:39:31 +0100

    Update ThisPC Shortcut

--- shortcuts/CreateLogoffShortCut.ps1
+++ shortcuts/CreateLogoffShortCut.ps1
@@ -3,9 +3,9 @@
 ##
 
 $shell = New-Object -comObject WScript.Shell
-$DesktopPath = [Environment]::GetFolderPath(“Desktop”)
-$ShortcutFile = “$DesktopPath\Logoff.lnk”
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+$ShortcutFile = "$DesktopPath\Logoff.lnk"
 $shortcut = $shell.CreateShortcut($ShortcutFile)
-$shortcut.TargetPath = “C:\Windows\System32\logoff.exe”
-$Shortcut.IconLocation = “%SystemRoot%\System32\SHELL32.dll, 44”
+$shortcut.TargetPath = "C:\Windows\System32\logoff.exe"
+$Shortcut.IconLocation = "%SystemRoot%\System32\SHELL32.dll, 44"
 $shortcut.Save()
